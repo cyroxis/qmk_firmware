7f91ea1cbce9c4a4a8bc1df991e2e16353c2e38e
Branden Wilson <bwilson@developertown.com>
2019-11-01 15:47:55 -0400

    Add Function keys

--- keyboards/crkbd/keymaps/cyroxis/keymap.c
+++ keyboards/crkbd/keymaps/cyroxis/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,              KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_LCTL,    LG(KC_A),   LA(KC_S),   LC(KC_D),   KC_F,       KC_G,              KC_H,       KC_J,       RC(KC_K),   RA(KC_L),   RG(KC_SCLN),KC_QUOT,   \
+     KC_GRAVE,   LG(KC_A),   LA(KC_S),   LC(KC_D),   KC_F,       KC_G,              KC_H,       KC_J,       RC(KC_K),   RA(KC_L),   RG(KC_SCLN),KC_QUOT,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_LSFT,    LC(KC_Z),   KC_X,       KC_C,       KC_V,       KC_B,              KC_N,       KC_M,       KC_COMM,    KC_DOT,     RC(KC_SLSH),KC_SFTENT, \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_TRNS,    KC_EXLM,    KC_AT,      KC_LPRN,    KC_RPRN,    KC_AMPR,           KC_PMNS,    KC_7,       KC_8,       KC_9,       KC_PSLS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_HASH,    KC_DLR,     KC_LBRC,    KC_RBRC,    KC_ASTR,           KC_PPLS,    KC_4,       KC_5,       KC_6,       KC_PAST,    KC_TRNS,   \
+     KC_PIPE,    KC_HASH,    KC_DLR,     KC_LBRC,    KC_RBRC,    KC_ASTR,           KC_PPLS,    KC_4,       KC_5,       KC_6,       KC_PAST,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|  
-     KC_TRNS,    KC_PERC,    KC_CIRC,    KC_LCBR,    KC_RCBR,    KC_TRNS,           KC_PDOT,    KC_1,       KC_2,       KC_3,       KC_PEQL,    KC_TRNS,   \
+     KC_TRNS,    KC_PERC,    KC_CIRC,    KC_LCBR,    KC_RCBR,    KC_BSLS,           KC_PDOT,    KC_1,       KC_2,       KC_3,       KC_PEQL,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
                                          KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_0       \
   //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
@@ -71,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_ESC,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_TRNS,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_TRNS,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
                                          KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS    \
   //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MOVE] = LAYOUT( \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_HOME,    KC_PGUP,    KC_PGDN,    KC_END,     KC_DELETE, \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
