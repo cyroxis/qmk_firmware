6047cb182385fa79d69122b0dbcba3722c2c09c4
Branden Wilson <bwilson@developertown.com>
2019-11-04 16:22:30 -0500

    Move left hand symbols to new layer

--- keyboards/crkbd/keymaps/cyroxis/keymap.c
+++ keyboards/crkbd/keymaps/cyroxis/keymap.c
@@ -19,13 +19,13 @@ extern uint8_t is_master;
 #define _LOWER 1
 #define _RAISE 2
 #define _MOVE 3
-#define _ADJUST 4
+#define _FUNC 4
+#define _ADJUST 5
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  MOVE,
   ADJUST,
   BACKLIT,
   RGBRST
@@ -35,6 +35,7 @@ enum custom_keycodes {
 #define L_LOWER (1U << _LOWER)
 #define L_RAISE (1U << _RAISE)
 #define L_MOVE (1U << _MOVE)
+#define L_FUNC (1U << _FUNC)
 #define L_ADJUST (1U << _ADJUST)
 
 enum macro_keycodes {
@@ -61,31 +62,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_LSFT,    LC(KC_Z),   KC_X,       KC_C,       KC_V,       KC_B,              KC_N,       KC_M,       KC_COMM,    KC_DOT,     RC(KC_SLSH),KC_SFTENT, \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-                                         KC_LGUI,    LOWER,      KC_SPC,            KC_SPC,     RAISE,      MOVE        \
+                                         MO(_FUNC),  LOWER,      KC_ENT,            KC_SPC,     RAISE,      MO(_MOVE)  \
   //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
   ),
 
   [_RAISE] = LAYOUT( \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_EXLM,    KC_AT,      KC_LPRN,    KC_RPRN,    KC_AMPR,           KC_PMNS,    KC_7,       KC_8,       KC_9,       KC_PSLS,    KC_TRNS,   \
+     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_PMNS,    KC_7,       KC_8,       KC_9,       KC_PSLS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_PIPE,    KC_HASH,    KC_DLR,     KC_LBRC,    KC_RBRC,    KC_ASTR,           KC_PPLS,    KC_4,       KC_5,       KC_6,       KC_PAST,    KC_TRNS,   \
+     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_PPLS,    KC_4,       KC_5,       KC_6,       KC_PAST,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|  
-     KC_TRNS,    KC_PERC,    KC_CIRC,    KC_LCBR,    KC_RCBR,    KC_BSLS,           KC_PDOT,    KC_1,       KC_2,       KC_3,       KC_PEQL,    KC_TRNS,   \
+     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_PDOT,    KC_1,       KC_2,       KC_3,       KC_PEQL,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-                                        KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_0        \
+                                         KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_0       \
   //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
   ),
 
   [_LOWER] = LAYOUT( \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_MINS,    KC_EXLM,    KC_AT,      KC_LPRN,    KC_RPRN,    KC_AMPR,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_UNDS,    KC_HASH,    KC_DLR,     KC_LBRC,    KC_RBRC,    KC_ASTR,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+     KC_PIPE,    KC_PERC,    KC_CIRC,    KC_LCBR,    KC_RCBR,    KC_BSLS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-                                        KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS     \
+                                         KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS    \
   //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
   ),
 
@@ -93,9 +94,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_HOME,    KC_PGUP,    KC_PGDN,    KC_END,     KC_DELETE, \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
-     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           A(KC_LEFT), KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   A(KC_RIGHT),   \
+     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           A(KC_LEFT), KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   A(KC_RGHT),\
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+  //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
+                                         KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS    \
+  //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
+  ),
+
+
+  [_FUNC] = LAYOUT( \
+  //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
+     KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+  //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
+     KC_TRNS,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
+  //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
+     KC_TRNS,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   \
   //|-----------+-----------+-----------+-----------+-----------+-----------|      |-----------+-----------+-----------+-----------+-----------+-----------|
                                          KC_TRNS,    KC_TRNS,    KC_TRNS,           KC_TRNS,    KC_TRNS,    KC_TRNS    \
   //                                    |-----------+-----------+-----------|      |-----------+-----------+-----------|
@@ -220,15 +234,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-    case MOVE:
-      if (record->event.pressed) {
-        layer_on(_MOVE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_MOVE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
     case ADJUST:
         if (record->event.pressed) {
           layer_on(_ADJUST);
@@ -265,6 +270,8 @@ const char *read_layer_state(void) {
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
   } else if (L_MOVE & layer_state) {
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Move");
+  } else if (L_FUNC & layer_state) {
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Func");
   } else if (L_LOWER & layer_state) {
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
   } else if (L_RAISE & layer_state) {
